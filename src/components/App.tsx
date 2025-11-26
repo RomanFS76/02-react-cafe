b99875d22b29b8e2f9811c0b1d5afd784e4cf04e
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-11-26 22:59:44 +0200

    add VoteOptions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
 import css from './App.module.css';
 import CafeInfo from './CafeInfo/CafeInfo';
+import VoteOptions from './VoteOptions/VoteOptions';
 
 export default function App() {
-  return <div className={css.app}>
-    <CafeInfo />
-  </div>;
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions />
+    </div>
+  );
 }
